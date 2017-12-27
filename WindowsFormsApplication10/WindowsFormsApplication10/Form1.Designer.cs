a9385d880caaab36a22147cbaf8b4da57798d1a0
Nirmala Rao Dhanawada <wep.proto@gmail.com>
2017-12-27 16:26:00 +0530

    Add files via upload

---
+++ WindowsFormsApplication10/WindowsFormsApplication10/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.keepMeAliveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setMeFreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Keep me alive";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(50, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1094, 515);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(564, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Set me free";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.keepMeAliveToolStripMenuItem,
+            this.setMeFreeToolStripMenuItem,
+            this.exitApplicationToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 114);
+            // 
+            // keepMeAliveToolStripMenuItem
+            // 
+            this.keepMeAliveToolStripMenuItem.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keepMeAliveToolStripMenuItem.Name = "keepMeAliveToolStripMenuItem";
+            this.keepMeAliveToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.keepMeAliveToolStripMenuItem.Text = "Keep Me Alive";
+            this.keepMeAliveToolStripMenuItem.Click += new System.EventHandler(this.keepMeAliveToolStripMenuItem_Click);
+            // 
+            // setMeFreeToolStripMenuItem
+            // 
+            this.setMeFreeToolStripMenuItem.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.setMeFreeToolStripMenuItem.Name = "setMeFreeToolStripMenuItem";
+            this.setMeFreeToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.setMeFreeToolStripMenuItem.Text = "Set Me Free";
+            this.setMeFreeToolStripMenuItem.Click += new System.EventHandler(this.setMeFreeToolStripMenuItem_Click);
+            // 
+            // exitApplicationToolStripMenuItem
+            // 
+            this.exitApplicationToolStripMenuItem.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitApplicationToolStripMenuItem.Name = "exitApplicationToolStripMenuItem";
+            this.exitApplicationToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.exitApplicationToolStripMenuItem.Text = "Exit Application";
+            this.exitApplicationToolStripMenuItem.Click += new System.EventHandler(this.exitApplicationToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.helpToolStripMenuItem.Text = "About";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 498);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Keep Me Alive";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem keepMeAliveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setMeFreeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitApplicationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication10/WindowsFormsApplication10/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+        }
+        protected void Displaynotify()
+        {
+            try
+            {
+                notifyIcon1.Icon = new System.Drawing.Icon(Path.GetFullPath(@"favicon.ico"));
+                notifyIcon1.Text = "Keeping your Display Alive";
+                notifyIcon1.Visible = true;
+                notifyIcon1.BalloonTipTitle = "Keeping Your Display Alive";
+                notifyIcon1.BalloonTipText = "The Application is now active.";
+                notifyIcon1.ShowBalloonTip(100);
+            }
+            catch (Exception ex1)
+            {
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ForceSystemAwake();
+        }
+        public static void ForceSystemAwake()
+        {
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_CONTINUOUS |
+                                                  NativeMethods.EXECUTION_STATE.ES_DISPLAY_REQUIRED |
+                                                  NativeMethods.EXECUTION_STATE.ES_SYSTEM_REQUIRED |
+                                                  NativeMethods.EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
+        }
+
+        public static void ResetSystemDefault()
+        {
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResetSystemDefault();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Displaynotify();
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Show(Control.MousePosition);
+
+        }
+
+        private void keepMeAliveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ForceSystemAwake();
+        }
+
+        private void setMeFreeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetSystemDefault();
+        }
+
+        private void openApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 h = new Form1();
+            h.Show();
+        }
+
+        private void exitApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Application is currently inactive. Please run the .exe again to keep the application active.", "Keep me Alive");
+            if (System.Windows.Forms.Application.MessageLoop)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                System.Environment.Exit(1);
+            }
+
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The 'Keep me Alive' App will help you to keep your display switched on without any interuption for infinite team just with a mouseclick. Done with your meeting? Restore back to normal in a second.", "About - Keep me Alive");
+        }
+    }
+    internal static partial class NativeMethods
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+    }
+
+}
